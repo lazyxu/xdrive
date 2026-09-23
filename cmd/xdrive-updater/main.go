0d0dab99a85783f0560da29d8ae3334ef36073f1
Xu Liang <isxuliang@gmail.com>
2026-09-24 00:24:34 +0800

    feat: make Linux updater channel-aware

--- cmd/xdrive-updater/main.go
+++ cmd/xdrive-updater/main.go
@@ -2,26 +2,46 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
+	"strings"
 
 	xupdate "github.com/lazyxu/xdrive/internal/update"
 	"github.com/lazyxu/xdrive/internal/version"
 )
 
 func main() {
+	channelFlag := flag.String("channel", "", "update channel: stable or master")
+	flag.Parse()
+
 	current := version.String()
-	if !xupdate.IsReleaseVersion(current) {
-		log.Printf("xDrive updater: %s is not a stable release build; skipping", current)
+	channel := strings.TrimSpace(*channelFlag)
+	var err error
+	if channel == "" {
+		channel, err = xupdate.AutomaticChannel(current)
+		if err != nil {
+			log.Printf("xDrive updater: %v", err)
+			return
+		}
+	}
+	if channel == "" {
+		log.Printf("xDrive updater: %s has no automatic channel; use --channel master for development builds", current)
 		return
 	}
-	started, result, err := xupdate.InstallLatest(context.Background(), current)
+	channel, err = xupdate.NormalizeChannel(channel)
+	if err != nil {
+		log.Printf("xDrive updater: %v", err)
+		return
+	}
+
+	started, result, err := xupdate.InstallChannel(context.Background(), current, channel)
 	if err != nil {
 		log.Printf("xDrive updater: %v", err)
 		return
 	}
 	if !started {
-		log.Printf("xDrive updater: %s is current", current)
+		log.Printf("xDrive updater: %s is current on %s channel", current, channel)
 		return
 	}
-	log.Printf("xDrive updater: installing %s", result.Latest)
+	log.Printf("xDrive updater: installing %s from %s channel", result.Latest, channel)
 }
