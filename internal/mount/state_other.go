d463a189207a10cf1d999c3ed904871fe3f4b8af
Xu Liang <isxuliang@gmail.com>
2026-09-23 23:58:22 +0800

    style: gofmt non-Windows state stubs

--- internal/mount/state_other.go
+++ internal/mount/state_other.go
@@ -4,9 +4,15 @@ package mount
 
 import "fmt"
 
-func keepLocalPlatform(string) error { return fmt.Errorf("file availability controls require Windows CfAPI") }
-func releaseSpacePlatform(string) error { return fmt.Errorf("file availability controls require Windows CfAPI") }
-func onlineOnlyPlatform(string) error { return fmt.Errorf("file availability controls require Windows CfAPI") }
+func keepLocalPlatform(string) error {
+	return fmt.Errorf("file availability controls require Windows CfAPI")
+}
+func releaseSpacePlatform(string) error {
+	return fmt.Errorf("file availability controls require Windows CfAPI")
+}
+func onlineOnlyPlatform(string) error {
+	return fmt.Errorf("file availability controls require Windows CfAPI")
+}
 func availabilityPlatform(path string) (FileAvailability, error) {
 	return FileAvailability{Path: path}, fmt.Errorf("file availability controls require Windows CfAPI")
 }
